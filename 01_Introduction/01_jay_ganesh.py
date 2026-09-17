db0add8070b8881e0df00d1e8025b13bf617e845
codingwithamar <codingwithamar@gmail.com>
2026-09-17 23:29:35 +0530

    some minor changes

--- 01_Introduction/01_jay_ganesh.py
+++ 01_Introduction/01_jay_ganesh.py
@@ -1,11 +1,21 @@
-'''
-This code created by codingwithamar@gmail.com
-path : python_system_programming/introduction/01_jay_ganesh.py
-Subject : python code Compile, Run, Bytecode Compile, Multiple Bytecode Compile
-for run python code use below commands :
-1. Python Program Run : python hello.py or py hello.py (we can use py for python and python3 replacement)
-2. Python Bytecode Compile : python -m py_compile hello.py
-3. Compile All Python Files in a Folder : python -m compileall .
-'''
+# =============================================================================
+# Author  : codingwithamar@gmail.com
+# File    : 01_jay_ganesh.py
+# Path    : Python-Programming-Language/01_Introduction/01_jay_ganesh.py
+# Subject : python code Compile, Run, Bytecode Compile, Multiple Bytecode Compile 
+#           for run python code use below commands :
+# Description : 
+# 1. Python Program Run : python hello.py or py hello.py (we can use py for python and python3 replacement)
+# 2. Python Bytecode Compile : python -m py_compile hello.py
+# 3. Compile All Python Files in a Folder : python -m compile all .
+# =============================================================================
 
-print("Jay Ganesh");
+def main():
+    print("Jay Ganesh");
+
+
+if __name__ == "__main__":
+    main()
+
+
+
