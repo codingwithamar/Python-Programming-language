5fa70bebc289745b06c11e3051e0d5b417ccaee9
codingwithamar <codingwithamar@gmail.com>
2026-09-23 13:59:50 +0530

    Arithmatic operator file added

---
+++ 05_Operators_in_python/00_Operators_in_python.py
@@ -0,0 +1,28 @@
+# =============================================================================
+# Author  : codingwithamar@gmail.com
+# File    : 0_Operators_in_python.py
+# Path    : Python-Programming-Language/05_Operators_in_python/0_Operators_in_python.py
+# Subject : Operators in Python
+# Description : Operators description
+# =============================================================================
+
+def main():
+    #----------------------------------------------------------------------------------------------------
+    #										'Operators in Python'
+    #----------------------------------------------------------------------------------------------------
+    #________________________________________________________________________________
+    #					    	'Arithmatic Operators'
+        print("List of Arithmatic Operators : ")
+        print("1. Addition and Unary Plus : " "( + )")
+        print("2. Substraction and Unary Minus : " "( - )")
+        print("3. Multiplication : " "( * )")
+        print("4. Division : " "( / )")
+        print("5. Floor Divison : " "( // )")
+        print("6. Modulus : " "( % )")
+        print("7. Power : " "( ** )")
+    #________________________________________________________________________________
+    
+
+
+if __name__ == "__main__":
+    main()
